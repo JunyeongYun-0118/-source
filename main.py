484252d7df5b43658f1ad44ea40df6b10f59805e
JunyeongYun-0118 <strush2704@gmail.com>
2025-03-18 13:19:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 import requests
 
@@ -5,14 +6,19 @@ app = FastAPI()
 
 GITHUB_JSON_URL = "https://raw.githubusercontent.com/JunyeongYun-0118/-source/main/sources.json"
 
-# 깃허브에서 JSON 데이터 가져오기
 def fetch_github_json():
     response = requests.get(GITHUB_JSON_URL)  
     if response.status_code == 200:
-        return response.json()  # JSON 데이터 반환
+        return response.json()
     else:
         return {"error": "Failed to fetch data from GitHub"}
 
 @app.get("/sources")
 async def get_sources():
     return fetch_github_json()
+
+# 서버 실행 코드
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Render에서 자동으로 포트 설정
+    uvicorn.run(app, host="0.0.0.0", port=port)
